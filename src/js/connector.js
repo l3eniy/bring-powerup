46a0b0377c113e6604f903ba3205928f4f3da6f4
l3eniy <pt!gD%f6%^01>
2022-05-15 17:50:21 +0200

    intial8

--- src/js/connector.js
+++ src/js/connector.js
@@ -14,5 +14,15 @@ window.TrelloPowerUp.initialize({
             callback: onBtnClick,
             condition: 'edit'
         }];
+    },
+    'show-settings': function(t, options){
+        // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
+        // what should Trello show. We highly recommend the popup in this case as
+        // it is the least disruptive, and fits in well with the rest of Trello's UX
+        return t.popup({
+            title: 'Bring anmelden',
+            url: './configure.html',
+            height: 184 // we can always resize later
+        });
     }
 });
